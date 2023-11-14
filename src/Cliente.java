8fb95d985a41bfe15a8fd79555250e6b34619b67
luizfilipee14 <91280826+luizfilipee14@users.noreply.github.com>
2023-11-14 20:53:20 -0300

    Implementando a classe Cliente.

---
+++ src/Cliente.java
@@ -0,0 +1,35 @@
+public class Cliente {
+    private int id;
+    private String email;
+    private String telefone;
+
+    public Cliente(int id, String email, String telefone) {
+        this.id = id;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
